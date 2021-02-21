bbd220444be086666ab44f28568b526efc6b50de
Willians Nieves <williamsnieves@gmail.com>
2021-02-21 16:30:43 +0100

    update cypress

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -2,10 +2,11 @@ describe('Dashboard suite test', () => {
 it('Dashboard deaths block', () => {
     cy.visit('https://williamsnieves.github.io/covid-dashboard/')
     cy.findByText(/Total deaths/i).should('exist')
-    cy.get(el).should('match.text', /\d+\W?/g);
+    cy.findByTestId('total-deaths').invoke('text').should('match', /\d+\W?/gm)
   })
   it('Dashboard people infected block', () => {
     cy.visit('https://williamsnieves.github.io/covid-dashboard/')
     cy.findByText(/People infected/i).should('exist')
+    cy.findByTestId('people-infected').invoke('text').should('match', /\d+\W?/gm)
   })
 })
